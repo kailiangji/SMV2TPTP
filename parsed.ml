df3a6532ea79a87f73fcd0cc273f1678fb8284d6
Kailiang Ji <kailiang.ji2013@gmail.com>
2016-01-13 22:50:00 +0100

    add properties to parsed.ml

--- parsed.ml
+++ parsed.ml
@@ -1,36 +1,33 @@
 type var = string
-type bvar = string
-type name = string
 
-type boolConst = True | False 
-
-type infix =
-  | Land | Lor | Limp | Leqv
-
-type lexpr =
-  | Neg of lexpr
+type exp =
   | True
   | False
-  | Infix of lexpr * infix * lexpr
-  | BVar of string
+  | Neg of exp
+  | And of exp * exp
+  | Or of exp * exp
+  | Eq of exp * exp
+  | Var of string
 
-type nlexpr =
-  |NBVar of string
-  |NBConst of string * string
-
-type var_type =
-  |Bvar of var
-  |NBvar of var * string list
-  |Process of var * name * var list
+type condition =
+  | Otherwise
+  | Eq of string * string
 
 type assign =
-  |BInit of bvar * lexpr 
-  |NBInit of var * nlexpr
-  |BNext of bvar * lexpr
-  |NBNext of var * (nlexpr * nlexpr) list
+  | Init of var * exp
+  | Next of var * exp
+  | CNext of var * (exp * exp) list
+
+type var_decl =
+  | Boolean of string
+  | Elem of string * string list
+  | Proc of string * string * string list
 
 type spec =
-  | Eq of bvar * boolConst
+  | Eq of string * exp
+  | Neg of spec
+  | And of spec * spec
+  | Or of spec * spec
   | AX of spec
   | EX of spec
   | AF of spec
@@ -41,13 +38,10 @@ type spec =
   | EU of spec * spec
   | AR of spec * spec
   | ER of spec * spec
-  | Or of spec * spec
-  | And of spec * spec
-  | Imp of spec * spec
-  | Equv of spec * spec
 
 type decl =
-  | Module1 of name * var_type list * assign list * spec
-  | Module2 of name * var list * var_type list * assign list
+  | Module1 of string * var_decl list * assign list * spec
+  | Module2 of string * string list * var_decl list * assign list
+
 
 type file = decl list
